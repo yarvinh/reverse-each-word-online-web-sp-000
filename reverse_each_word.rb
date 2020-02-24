6860f94f5b2c56913462ed0e9c7f806dec35b649
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-24 21:48:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
    array = sentence.split
    array.map do |words|
     reverse_sentence = words.reverse
-    reverse_sentence
+    return reverse_sentence
    end
 end
