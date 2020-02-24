4ad25e4d8398c672911841a1273552686c2b044e
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-24 21:47:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
    array = sentence.split
    array.map do |words|
     reverse_sentence = words.reverse
-    reverse_sentence.join
+    reverse_sentence
    end
 end
